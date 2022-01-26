221cdde3e0e58fd0184c2b00dff822a1aee82a4f
KyouStg <kit115kat624@gmail.com>
2022-01-26 16:30:26 +0900

    update python

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\kit11\source\repos\DeployTest\DeployTest\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
